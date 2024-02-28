09d68dff46474dd76aa78d1484f9106298b4a738
MikaelDv <enzomikael@hotmail.com>
2024-02-27 21:31:06 -0300

    Novos exercícios

---
+++ ex0.js
@@ -0,0 +1,8 @@
+let nome = prompt('Digite o seu nome:');
+let idade = prompt('Digite a sua idade:');
+
+if (idade >= 18){
+    alert(`${nome} você pode tirar a habilitação!`)
+}else{
+    alert(`${nome} você não pode tirar a habilitação!`)
+}
